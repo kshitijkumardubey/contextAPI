29c57c111434a6b9e88bce9ee4925a43182e8135
Kshitij <dkshitijkumar98@gmail.com>
2020-09-06 17:17:27 +0530

    basic theme switcher using contextAPI

---
+++ src/components/ThemeToggler.js
@@ -0,0 +1,21 @@
+import React ,{useContext} from "react"
+import ThemeContext from "../context/ThemeContext"
+
+
+const ThemeToggler = () =>{
+
+    const [themeMode,setThemeMode] = useContext(ThemeContext)
+     
+    return(
+        <div
+        onClick={()=>{
+            setThemeMode(themeMode ===  "lght" ? "dark" :"light")
+        }}
+        >
+         <span>{themeMode === "light" ?"Turn Off":"Lights On"}</span>
+        </div>
+    )
+}
+
+
+export default ThemeToggler;
